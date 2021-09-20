ce61dfa2701e5692f9ab31180daf3754c4ff0248
Gabrielė Dilytė <GDI@tiatechnology.com>
2021-09-20 20:03:55 +0300

    Added rows input field and diamond button

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
 const allButtons = document.querySelectorAll("#shapes button");
-const rows = 6;
 
-function print(option) {
+function print(option, rows) {
   switch (option) {
     case "triangle":
       for (let i = 1; i <= rows; i++) {
         document.querySelector("#output").innerHTML += "*".repeat(i) + "<br />";
       }
       break;
-    /*for (let i = 1; i <= 6; i++) {
-        for (let j = 1; j <= i; j++) {
-          document.querySelector("#output").innerHTML += "*";
-        }
-        document.querySelector("#output").innerHTML += "<br />";
-      }*/
     case "pyramid":
       for (let i = 1; i <= rows; i++) {
         document.querySelector("#output").innerHTML +=
-          "&nbsp".repeat(rows - i) +
-          ("*" + "&nbsp").repeat(i) +
-          "&nbsp".repeat(rows - i) +
-          "<br />";
+          "&nbsp".repeat(rows - i) + ("*" + "&nbsp").repeat(i) + "<br />";
       }
       break;
     case "reverse_pyramid":
       for (let i = rows; i >= 1; i--) {
         document.querySelector("#output").innerHTML +=
-          "&nbsp".repeat(rows - i) +
+          "&nbsp".repeat(rows - i) + ("*" + "&nbsp").repeat(i) + "<br />";
+      }
+      break;
+    case "diamond":
+      const topRows = Math.ceil(rows / 2);
+      const bottomRows = topRows - 1;
+
+      for (let i = 1; i <= topRows; i++) {
+        document.querySelector("#output").innerHTML +=
+          "&nbsp".repeat(topRows - i) + ("*" + "&nbsp").repeat(i) + "<br />";
+      }
+      for (let i = bottomRows; i >= 1; i--) {
+        document.querySelector("#output").innerHTML +=
+          "&nbsp".repeat(bottomRows - i + 1) +
           ("*" + "&nbsp").repeat(i) +
-          "&nbsp".repeat(rows - i) +
           "<br />";
       }
       break;
@@ -42,6 +44,7 @@ function print(option) {
 
 allButtons.forEach((element) => {
   element.addEventListener("click", (event) => {
-    print(event.target.id);
+    const rows = Number(document.querySelector("#rowsNumber").value);
+    print(event.target.id, rows);
   });
 });
